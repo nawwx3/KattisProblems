5f7859f3142f04a20572f8f9ab908d2c1af3ba43
Nathan Welch <Nathan Welch>
2017-02-23 17:56:38 -0600

    Adding a file Figuring out how this works adding a file in a folder in a folder

---
+++ arealchallenge/src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*
+ * Created by Nathan Welch on 2/23/2017.
+ * A real Challenge - Kattis Problem
+ */
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double i = (double)sc.nextInt();
+        double value = Math.sqrt(i)*4;
+        System.out.println(value);
+    }
+}
+
+
+
